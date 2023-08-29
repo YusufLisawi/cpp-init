f93f7f8a4e751aedba70bbd569ee3146fb8d9524
YusufLisawi <yusufisawi@gmail.com>
2023-08-29 15:10:45 +0100

    Added updates

--- cpp-create.sh
+++ cpp-create.sh
@@ -65,4 +65,38 @@ $class_name& $class_name::operator=(const $class_name& other) {
 }
 EOF
 
+# Generate Makefile if it doesn't exist
+makefile="$directory/Makefile"
+
+if [[ ! -f "$makefile" ]]; then
+    echo "Generating $makefile..."
+    cat > $makefile << EOF
+NAME = $class_name
+
+SRCS = main.cpp
+
+OBJS = \$(SRCS:.cpp=.o)
+
+CXX = c++
+
+CXXFLAGS = -Wall -Wextra -Werror -std=c++98
+
+all: \$(NAME)
+
+\$(NAME): \$(OBJS)
+	\$(CXX) \$(CXXFLAGS) \$(OBJS) -o \$(NAME)
+
+%.o: %.cpp
+	\$(CXX) \$(CXXFLAGS) -c \$< -o \$@
+
+clean:
+	rm -f \$(OBJS)
+
+fclean: clean
+	rm -f \$(NAME)
+
+re: fclean all
+EOF
+fi
+
 echo "Done."
